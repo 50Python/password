0b0b9e09d6edb7ee730ccab0e84077d2fb9fc8a7
50Python <C931004@mail4u.tun.edu.tw>
2023-10-30 18:02:22 +0800

    fix input times

--- password.py
+++ password.py
@@ -5,7 +5,7 @@ while error > 0: #設定 while loop 次數如果大於0、則繼續執行
 	password = input('請輸入密碼: ')
 	#password = int(password) #取消轉化字串為 int
 	if password != key: #假如輸入的 password 不等於一開始設定的密碼
-		print('密碼錯誤、還可以輸入:' , error , '次') #印出剩餘次數
+		print('密碼錯誤、還可以輸入:' , error - 1 , '次') #印出剩餘次數
 		error = error - 1 # error 變數值減 1
 	else:
 		print('登入成功')
