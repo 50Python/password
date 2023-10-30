5b77e61b5ceee5c20b87329c7ffc3f5d2cd3ea55
50Python <C931004@mail4u.tun.edu.tw>
2023-10-30 17:41:00 +0800

    update setting password

--- password.py
+++ password.py
@@ -1,10 +1,11 @@
-key = 1234
+
+key = input('請設定密碼: ')
 error = 3
 while error > 0:
 	password = input('請輸入密碼: ')
-	password = int(password)
+	#password = int(password)
 	if password != key:
-		print('密碼錯誤、還可以輸入:' , error)
+		print('密碼錯誤、還可以輸入:' , error , '次')
 		error = error - 1
 	else:
 		print('登入成功')
